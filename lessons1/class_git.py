6f30b24f1001e319b71c3b6e3358ffec386aeaec
you you <my@my.my>
2020-02-03 16:52:38 +0700

    Added: 1) logging 2) create repos 3) delete repos 4) auth 5) print repos 6) export repos

--- lessons1/class_git.py
+++ lessons1/class_git.py
@@ -4,6 +4,7 @@ import requests
 import json
 import os
 from time import gmtime, strftime
+import logging
 
 class gitApi:
 
@@ -13,46 +14,129 @@ class gitApi:
 
     # Dicts urls for generate template requests
     __urls = {
-      'repos':'https://api.github.com/users/{}/repos',
+        'list_repos': 'https://api.github.com/users/{}/repos',
+        'create_repos': 'https://api.github.com/user/repos',
+        'delete_repos': 'https://api.github.com/repos/{}/{}'
     }
 
     __toSaveDumps = 'dumps'
+    logger = None
+    __logging_filename = 'lessons1.log'
 
     # Parametrs for auth
-    __username = None
-    __token = None
+    username = None
+    __passwd = None
+    __sessions = None
 
-    def __init__(self, username = None, token = None):
-        self.__username = username
-        self.__token = token
+    def __init__(self, username = None, passwd = None):
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        fh = logging.FileHandler(self.__logging_filename)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        self.logger.addHandler(fh)
+
+        if username:
+            self.username = username
+        if passwd:
+            self.__passwd = passwd
         super().__init__()
+
+    def __del__(self):
+        self.username = None
+        self.__passwd = None
+
+    def set_auth(self, username, passwd):
+        self.username = username
+        self.__passwd = passwd
+
+    def __get_sessions(self):
+        if not self.username or not self.__passwd:
+            self.logger.error('Не заполнен логин или пароль')
+            Exception('Не заполнен логин или пароль')
+
+        session = None
+
+        try:
+            session = requests.Session()
+            session.auth = (self.username, self.__passwd)
+        except Exception as e:
+            self.logger.error(f'Ошибка: {e}')
+            print(e)
+
+        return session
+
+    def create_repos(self, name):
+        session = self.__get_sessions()
+        issue = {
+                "name": name,
+                "description": "This is your first repository",
+                "homepage": "https://github.com",
+                "private": False,
+                "has_issues": True,
+                "has_wiki": True
+        }
+
+        response = session.post(self.__urls['create_repos'], json.dumps(issue))
+
+        if response.ok:
+            self.logger.info(f'Репозиторий успешно создан! Склонировать можно: git clone {json.loads(response.text)["clone_url"]}')
+            return {'create': True, 'msg': json.loads(response.text)['clone_url']}
+        else:
+            self.logger.error(f'Ошибка создания репозитория! Ошибка: {json.loads(response.text)["message"]}')
+            return {'create': False, 'msg': json.loads(response.text)['message']}
+
+    def delete_repos(self, name):
+        session = self.__get_sessions()
+
+        urls = self.__urls['delete_repos'].format(self.username, name)
+        response = session.delete(urls)
+        if response.ok:
+            self.logger.info(f'Репозиторий успешно удален! URL: {urls}')
+            return {'delete': True, 'msg': 'Репозиторий успешно удален!'}
+        else:
+            self.logger.error(f'Ошибка удаления: {json.loads(response.text)["message"]}')
+            return {'delete': False, 'msg': json.loads(response.text)['message']}
 
     def print_repos(self, username):
         data = self.get_repos(username)
         if data['isData']:
+            self.logger.info(f"В репозитории найдено {len(data['response'])} проект(ов)")
             print("%-60s%30s%30s%10s%-100s" % ("Наименование проекта", "Дата создания репозитория", "Последняя дата изменения", '', "Ссылка на git_clone"))
             for project in data['response']:
                 print("%-60s%30s%30s%10s%-100s" % (project['name'], project['created_at'], project['updated_at'], '', project['clone_url']))
         else:
+            self.logger.error(f"В репозитории пользователя {username} не найдено проектов")
             print(f"В репозитории пользователя {username} не найдено проектов")
 
     def export_repos(self, username):
         data = self.get_repos(username)
         if data['isData']:
+            self.logger.info(f"В репозитории найдено {len(data['response'])} проект(ов)")
             if not os.path.exists(self.__toSaveDumps):
                 try:
                     os.makedirs(self.__toSaveDumps)
                 except OSError as err:
+                    self.logger.error(f'Ошибка удаления: {err}')
                     print(err)
             date_now = strftime('%Y%m%d_%H%M%S', gmtime())
             with open(f"{self.__toSaveDumps}/dump_{username}_{date_now}.json", 'w+') as f:
                 json.dump(data['response'], f)
         else:
+            self.logger.error(f"В репозитории пользователя {username} не найдено проектов")
             print(f"В репозитории пользователя {username} не найдено проектов")
 
     def get_repos(self, username):
         try:
-            response = requests.get(self.__urls['repos'].format(username))
+            response = requests.get(self.__urls['list_repos'].format(username))
             return {'isData': True if response.ok else False, 'response': json.loads(response.text)}
         except Exception as e:
+            self.logger.error(f'Ошибка получения репозиториев: {e}')
             print(e)
+
+    def isAuth(self):
+        if self.__passwd and self.username:
+            return True
+        else:
+            return False
--- lessons1/main.py
+++ lessons1/main.py
@@ -1,8 +1,34 @@
 # -*- coding: utf-8 -*-
-import datetime
-
 from class_git import gitApi
+from getpass import getpass
 
 ga = gitApi()
+
+print("-" * 10)
+print('Урок 1')
+print("-" * 10)
+print("В качетсве работы с api для 1-го и 2-го задания был выбран github")
+print("-" * 10)
+while(True):
+    print("""1. Вывести список репозиториев для конкретного пользователя.\n2. Сохранить список репозиториев в формате json.""")
+    if ga.isAuth():
+        print("""3. Вывести список своих репозиториев.\n4. Вывести список своих репозиториев.\n5. Создать репозиторий.\n6. Удалить репозиторий .\n""")
+    else:
+        print("3. Авторизироваться в github с помощью логин/пароль")
+    variable = input("Выбирите один из пунктов: " )
+#
+# username = input('Login: ')
+# passwd = getpass(prompt='Password: ')
+#
+#
+# # create repos
+# create_resp = ga.create_repos('ttt')
+# delete repos
+# del_resp = ga.delete_repos('ttt')
+# print my repos
+# my_resp = ga.print_repos(ga.username)
+
+# print random repos
+# ga = gitApi()
 # ga.print_repos('asoifdhuipsdhuifhashfeaiushdui')
-ga.print_repos('WhiteAndBlackFox')
+# ga.print_repos('WhiteAndBlackFox')
